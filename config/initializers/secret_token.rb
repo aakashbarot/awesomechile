c4fc66af563c3d3866678be7783a8140880085fc
Aakash <abmrkt@gmail.com>
2014-10-11 17:28:12 -0300

    initial commit of the Awesome chile app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awesomechile::Application.config.secret_key_base = 'e3a6d17368cc142263bf75937f880eda4d7986d9475adf3647393c1e67153757c7a0e0e0fd3958dd15bea05761d56684325ea1c8ae595c0cae38038fa23f0bd5'
